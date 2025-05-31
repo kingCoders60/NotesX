102e05f7b46576bb51d4008b5f4d09f3c8f29cc8
kingCoders60 <spworld30@gmail.com>
2025-06-01 02:53:52 +0530

    done

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
-import RateLimitedUI from "../components/RateLimitedUI";
+// import RateLimitedUI from "../components/RateLimitedUI";
 import axios from "axios";
 import toast from "react-hot-toast";
 import NoteCard from "../components/NoteCard"
@@ -35,14 +35,14 @@ const HomePage = () => {
   return (
     <div className="min-h-screen">
       <Navbar />
-      {isRateLimited && <RateLimitedUI />}
+      {/* {isRateLimited && <RateLimitedUI />} */}
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {isLoading && (
           <div className="flex justify-center items-center py-6">
             <span className="loading loading-dots loading-lg text-green-600 animate-pulse"></span>
           </div>
         )}
-        {notes.length ===0 &&!isRateLimited && <NotesNotFound/>}
+        {notes.length ===0 /*&&!isRateLimited*/ && <NotesNotFound/>}
 
         {notes.length > 0 && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
