a5aa157d8040beda6dcd38b076a102bffc27424c
kingCoders60 <spworld30@gmail.com>
2025-07-01 12:39:50 +0530

    minor bug fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
-app.use(rateLimiter);
+
 
 // our simple custom middleware
 // app.use((req, res, next) => {
